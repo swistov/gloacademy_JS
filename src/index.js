c89927fdecc66eb48b71ad2bd7bf2739e8e11101
Svistov Nikolay <swistov@gmail.com>
2019-08-13 22:22:23 +0300

    First lesson

---
+++ src/index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// chekbox
+
+const checkbox = document.querySelectorAll('.filter-check_checkbox');
+
+checkbox.forEach(function(element) {
+    element.addEventListener('change', function() {
+        if (this.checked) {
+            this.nextElementSibling.classList.add('checked');
+        } else {
+            this.nextElementSibling.classList.remove('checked');
+        }
+    });
+});
+
+// Корзина
+const btnCart = document.getElementById('cart');
+const modalCart = document.querySelector('.cart');
+const closeBtn = document.querySelector('.cart-close');
+
+btnCart.addEventListener('click', () => {
+    modalCart.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+});
+
+closeBtn.addEventListener('click', () => {
+    modalCart.style.display = 'none';
+    document.body.style.overflow = '';
+});
+
+// Конец корзины
+
+// Работа с товаром
+
+const cards = document.querySelectorAll('.goods .card'),
+    cartWrapper = document.querySelector('.cart-wrapper'),
+    cartEmpty = document.getElementById('cart-empty'),
+    countGoods = document.querySelector('.counter');
+
+cards.forEach((card) => {
+    const btn = card.querySelector('button');
+
+    btn.addEventListener('click', () => {
+        const cardClone = card.cloneNode(true);
+        cartWrapper.appendChild(cardClone);
+        cartEmpty.remove();
+        showData();
+    });
+});
+
+function showData() {
+    const cardsCart = cartWrapper.querySelectorAll('.card');
+    countGoods.textContent = cardsCart.length;
+}
+
+// Конец работы с товаром   
