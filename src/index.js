b17e6e356a1d6a2cde1d93618def64ddeb31a463
Svistov Nikolay <swistov@gmail.com>
2019-08-15 22:27:26 +0300

    Lessosn 3

--- src/index.js
+++ src/index.js
@@ -117,21 +117,26 @@ function actionPage() {
     });
 
     // Фильтр по цене
-    min.addEventListener('change', filterPrice);
-    max.addEventListener('change', filterPrice);
+    discountCheckbox.addEventListener('click', filter);
+    min.addEventListener('change', filter);
+    max.addEventListener('change', filter );
  
-    function filterPrice() {
+    function filter(){
         cards.forEach((card) => {
             const cardPrice = card.querySelector('.card-price');
             const price = parseFloat(cardPrice.textContent);
+            const discount = card.querySelector('.card-sate');
 
             if ((min.value && price < min.value) || (max.value && price > max.value)) {
-                card.parentNode.remove();
+                card.parentNode.style.display = 'none';
+            } else if (discountCheckbox.checked &&  !discount) {
+                card.parentNode.style.display = 'none';
             } else {
                 card.parentNode.style.display = '';
             }
         });
     }
+    // Конец фильтра по цене
 
     // Поиск
     searchBtn.addEventListener('click', () => {
@@ -145,12 +150,99 @@ function actionPage() {
             }
         });
     });
+    
 }
-
-
 // Конец фильтр акции
 
-toggleCheckbox();
-toggleCart();
-addCart();
-actionPage();
+// Получение данных с сервера
+function getData() {
+    const goodsWrapper = document.querySelector('.goods');
+    return fetch('../db/db.json')
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error ('Данные не загружены, ошибка: ' + response.status);
+            }
+        })
+        .then((data) => {
+            return data;
+        })
+        .catch((err) => {
+            console.warn(err);
+            goodsWrapper.innerHTML = '<div style="color:red; font-size:30px">Упс что-то пошло не так</div>';
+        });
+
+}
+
+// Выводим карточки товара
+function renderCards(data){
+    const goodsWrapper = document.querySelector('.goods');
+    data.goods.forEach((good) => {
+        const card = document.createElement('div');
+        card.className = 'col-12 col-md-6 col-lg-4 col-xl-3';
+        card.innerHTML = `
+                <div class="card" data-category="${good.category}">
+                ${good.sale ? '<div class="card-sale">🔥Hot Sale🔥</div>' : ''}
+                    <div class="card-img-wrapper">
+                        <span class="card-img-top"
+                            style="background-image: url('${good.img}')"></span>
+                    </div>
+                    <div class="card-body justify-content-between">
+                        <div class="card-price" style="${good.sale ? 'color:red' : ''}">${good.price} ₽</div>
+                        <h5 class="card-title">${good.title}</h5>
+                        <button class="btn btn-primary">В корзину</button>
+                    </div>
+                </div>
+        `;
+        goodsWrapper.appendChild(card);
+    });
+}
+
+function renderCatalog(){
+    const cards = document.querySelectorAll('.goods .card');
+    const catalogList = document.querySelector('.catalog-list');
+    const catalogWrapper = document.querySelector('.catalog');
+    const catalogBtn = document.querySelector('.catalog-button');
+    const categories = new Set();
+
+    cards.forEach((card) => {
+        categories.add(card.dataset.category);
+    });
+
+    categories.forEach((item) => {
+        const li = document.createElement('li');
+        li.textContent = item;
+        catalogList.appendChild(li);
+    });
+
+    catalogBtn.addEventListener('click', () => {
+        if (catalogWrapper.style.display) {
+            catalogWrapper.style.display = '';
+        } else {
+            catalogWrapper.style.display = 'block';
+        }
+
+        if (event.target.tagName === 'LI'){
+            cards.forEach((card) => {
+                if (card.dataset.category === event.target.textContent) {
+                    card.parentNode.style.display = '';
+                } else {
+                    card.parentNode.style.display = 'none';
+                }
+            });
+        }
+    });
+}
+
+getData().then((data) => {
+    renderCards(data);
+    toggleCheckbox();
+    toggleCart();
+    addCart();
+    actionPage();
+    renderCatalog();
+});
+// Конец получения данных
+
+
